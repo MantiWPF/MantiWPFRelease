0ea907666676378fc264ca453e0d50fb25a7391e
MantiWPF <hefescripter@gmail.com>
2024-08-29 17:00:22 +0300

    Create sex.lua

---
+++ sex.lua
@@ -0,0 +1,8 @@
+--coded getcustomasset and watermark, ik you find it from autoexecute, idc if you change it too 🤯👍
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "MantiWPF",
+    Icon = "rbxassetid://18956441273",
+    Text = "MantiWPF v3.1",
+    Duration = 7    
+})
+print("MantiWPF is injected!")
